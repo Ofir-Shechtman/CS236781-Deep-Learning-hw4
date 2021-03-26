b9ff72798d9c3964d2e3f51d56fd4d86efc5f8ae
Ofir Shechtman <ofir.s@lambda.csf.technion.ac.il>
2021-03-26 11:27:43 +0300

    check gan on cpu

--- project/train.py
+++ project/train.py
@@ -2,7 +2,6 @@ import torch
 import torch.nn as nn
 from typing import Callable
 import os
-from .BushDataLoader import BushDataLoader
 from torch.utils.data import DataLoader
 from torch.optim.optimizer import Optimizer
 from .gan import Discriminator, SNDiscriminator, Generator
@@ -13,6 +12,7 @@ import numpy as np
 import sys
 import matplotlib.pyplot as plt
 import cs236781.plot as plot
+from .inception_score import inception_score
 
 
 def discriminator_loss_fn(y_data, y_generated, data_label=0, label_noise=0.0):
@@ -31,6 +31,7 @@ def discriminator_loss_fn(y_data, y_generated, data_label=0, label_noise=0.0):
     :return: The combined loss of both.
     """
     assert data_label == 1 or data_label == 0
+    print(y_generated)
     # TODO:
     #  Implement the discriminator loss.
     #  See pytorch's BCEWithLogitsLoss for a numerically stable implementation.
@@ -42,6 +43,7 @@ def discriminator_loss_fn(y_data, y_generated, data_label=0, label_noise=0.0):
 
     r1, r2 = 0 - label_noise / 2, 0 + label_noise / 2
     labels = torch.distributions.uniform.Uniform(r1, r2).sample(y_data.shape)
+    print(y_generated)
     loss_generated = bce(y_generated, labels.to(device=y_generated.device))
 
     return loss_data + loss_generated
@@ -87,7 +89,6 @@ def train_batch_discriminator(
         x_data: DataLoader,
 ):
     gen_data = gen_model.sample(len(x_data))
-
     dsc_loss = dsc_loss_fn(dsc_model(x_data), dsc_model(gen_data))
     dsc_loss.backward()
     dsc_optimizer.step()
@@ -103,6 +104,8 @@ def w_train_batch_discriminator(
         n_critic=5,
         c=0.01
 ):
+    for p in dsc_model.parameters():
+        p.requires_grad = True
     batch_size = x_data.shape[0]
     dsc_batch_losses = []
     # Discrimnator update, n_critic iterations
@@ -116,7 +119,7 @@ def w_train_batch_discriminator(
         y_generated = dsc_model(fake_imgs)
         dsc_loss_real, dsc_loss_fake = dsc_loss_fn(y_data, y_generated)
         dsc_loss_real.backward()
-        dsc_loss_fake.backward(torch.Tensor([-1]).to(dsc_model.device))
+        dsc_loss_fake.backward(torch.Tensor([-1]).to(x_data.device))
         dsc_iter_loss = dsc_loss_real - dsc_loss_fake
         dsc_batch_losses.append(dsc_iter_loss.item())
         dsc_optimizer.step()
@@ -132,15 +135,18 @@ def train_batch_generator(
         gen_optimizer: Optimizer,
         x_data: DataLoader,
 ):
+    for p in dsc_model.parameters():
+        p.requires_grad = False
+    gen_model.zero_grad()
     gen_data = gen_model.sample(len(x_data), with_grad=True)
 
     gen_loss = gen_loss_fn(dsc_model(gen_data))
     gen_loss.backward()
     gen_optimizer.step()
+    
+
 
     return gen_loss
-
-
 
 def train_batch(
         dsc_model: Discriminator,
@@ -189,43 +195,47 @@ def train_batch(
 
     return dsc_loss.item(), gen_loss.item()
 
+def dsc_loss_fn(self, y_data, y_generated):
+        return w_discriminator_loss_fn(y_data, y_generated)
+
 
 class Trainer:
-    def __init__(self, hp, dsc_cls, gen_cls, name='gan', wgan=False):
+    def __init__(self, hp, dl_train, dsc_cls, gen_cls, name='gan', wgan=False):
         self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
         batch_size = hp['batch_size']
         z_dim = hp['z_dim']
         # Data
-        self.dl_train = BushDataLoader(batch_size, shuffle=True)
+        self.dl_train = dl_train
         # im_size = self.dl_train.im_size
 
         # Model
         self.dsc = dsc_cls(self.dl_train.im_size).to(self.device)
         self.gen = gen_cls(z_dim, featuremap_size=4).to(self.device)
-        print(self.gen)
-        print(self.dsc)
 
         self.dsc_optimizer = self.create_optimizer(self.dsc.parameters(), hp['discriminator_optimizer'])
         self.gen_optimizer = self.create_optimizer(self.gen.parameters(), hp['generator_optimizer'])
-        print(self.dsc_optimizer)
 
         # Loss
         if wgan:
             self.dsc_loss_fn = wgan_discriminator_loss_fn
             self.gen_loss_fn = wgan_generator_loss_fn
         else:
-            self.dsc_loss_fn = lambda y_data, y_generated: discriminator_loss_fn(y_data, y_generated,
-                                                                                 hp['data_label'], hp['label_noise'])
+            #self.dsc_loss_fn = lambda y_data, y_generated: discriminator_loss_fn(y_data, y_generated,
+            #                                                                     hp['data_label'], hp['label_noise'])
             self.gen_loss_fn = lambda y_generated: generator_loss_fn(y_generated, hp['data_label'])
             
 
         # Training
-        self.checkpoint_file = f'checkpoints/{name}'
-        self.checkpoint_file_final = f'{self.checkpoint_file}_final'
-        if os.path.isfile(f'{self.checkpoint_file}.pt'):
-            os.remove(f'{self.checkpoint_file}.pt')
+        self.checkpoint_file = f'checkpoints/{name}.pt'
+        self.checkpoint_file_final = f'checkpoints/{name}_final.pt'
+        if os.path.isfile(self.checkpoint_file):
+            os.remove(self.checkpoint_file)
         self.n_critic = hp['n_critic']
         self.c = hp['c']
+        
+    def dsc_loss_fn(self, y_data, y_generated):
+        return discriminator_loss_fn(y_data, y_generated, 1.0, 0.1)
+
 
     # Optimizer
     @staticmethod
@@ -235,12 +245,11 @@ class Trainer:
         opt_params.pop('type')
         return optim.__dict__[optimizer_type](model_params, **opt_params)
 
-    def train(self):
-        num_epochs = 400
-        max_inc_score = 0
+    def train(self, num_epochs):
         self.dsc.train(True)
         self.gen.train(True)
-        self.inc_score_epochs=0
+        self.inc_score_epochs=100
+        max_inc_score = 0.
         try:
             dsc_avg_losses, gen_avg_losses = [], []
             for epoch_idx in range(num_epochs):
@@ -275,20 +284,20 @@ class Trainer:
                 plt.close(fig)
 
                 if num_epochs - epoch_idx <= self.inc_score_epochs:
-                    print("Calculating inception score...")
-                    #inc_score = 0#calc_inception_score(self.gen_model)
-                    #print(f'Epoch {epoch_idx + 1}, Inception score:{inc_score}')
-                    #if inc_score > max_inc_score:
-                    #    max_inc_score = inc_score
-                    #    torch.save(self.gen_model, self.checkpoint_file)
-                    #    print(f'Saved checkpoint.')
+                    inc_score, _ = inception_score(self.gen.sample(100, with_grad=False), resize=True)
+                    print(f'Epoch {epoch_idx + 1}, Inception score:{inc_score}')
+                    if inc_score > max_inc_score:
+                        max_inc_score = inc_score
+                        torch.save(self.gen, self.checkpoint_file)
+                        print(f'Saved checkpoint.')
             print(f"Finished training! best inception score is: {max_inc_score}")
             return max_inc_score
         except KeyboardInterrupt as e:
             print('\n *** Training interrupted by user')
-        finally:
-            self.dsc.eval()
-            self.gen.eval()
+        #finally:
+        #    self.dsc.eval()
+        #    self.gen.eval()
+            
 
 
 
@@ -321,15 +330,19 @@ class Trainer:
 
 
 class GANTrainer(Trainer):
-    def __init__(self, hp):
-        super(GANTrainer, self).__init__(hp, Discriminator, Generator, name='gan', wgan=False)
+    def __init__(self, hp, dl_train):
+        super().__init__(hp, dl_train, Discriminator, Generator, name='gan', wgan=False)
 
 
 class SNGANTrainer(Trainer):
-    def __init__(self, hp):
-        super().__init__(hp, SNDiscriminator, Generator, name='sngan', wgan=False)
+    def __init__(self, hp, dl_train):
+        super().__init__(hp, dl_train, SNDiscriminator, Generator, name='sngan', wgan=False)
 
 
 class WGANTrainer(Trainer):
-    def __init__(self, hp):
-        super().__init__(hp, Discriminator, Generator, name='wgan', wgan=True)
+    def __init__(self, hp, dl_train):
+        super().__init__(hp, dl_train, Discriminator, Generator, name='wgan', wgan=True)
+        
+class SNWGANTrainer(Trainer):
+    def __init__(self, hp, dl_train):
+        super().__init__(hp, dl_train, SNDiscriminator, Generator, name='snwgan', wgan=True)
